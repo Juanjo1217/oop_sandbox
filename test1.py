700f4b2d8f38154cf09298c0eebec04707cce54a
Ubuntu <jjaragon@oop-jjaragon.b200ocpmchqehipu4oipqd4ree.phxx.internal.cloudapp.net>
2022-08-30 14:02:28 +0000

    adding test file

--- test1.py
+++ test1.py
@@ -1,1 +1,45 @@
-print("hello world")
+from unicodedata import category
+from random import randint
+
+class Budgets:
+    def __init__(self, category: str,
+        innitial_balance: float,
+        running_balance: float) -> None:
+        pass
+        self.category = category
+        self.innitial_balance = innitial_balance
+        #private atribute
+        self.running_balance = running_balance
+
+    def get_running_balance(self) -> float:
+        return self.running_balance
+        
+        
+    def withdraw(self, amount:float):
+        self.running_balance = self.running_balance - amount
+        
+
+    def deposit(self, amount:float):
+        self.running_balance = self.running_balance + amount
+        
+class User:
+    def __init__(self, name: str, total_budget: float) -> None:
+        self.name = name
+        self.total_budget = total_budget
+        self.id = randint(0, 1000)
+        self.budgets = dict
+    
+    
+
+    def add_budget(self, Category:str,
+        innitial_balance: float,
+        running_balance: float):
+        """
+        create a budgets category
+        """
+        budget = Budgets(category = category,
+                    innitial_balance = innitial_balance)
+        self.budgets.update({category: budget})
+
+    def transfer_balance(self, sender: str, raceiver: str, amount: float):
+        self.budgets[sender]
